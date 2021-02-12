b061f000339bed099bdd053586be7ae48beb04dd
Tej <codewithtej@gmail.com>
2021-02-12 14:22:00 -0500

    Intial Commit

---
+++ app/src/main/java/com/codewithtej/maps_gurtej_c0782510/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codewithtej.maps_gurtej_c0782510;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/codewithtej/maps_gurtej_c0782510/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.codewithtej.maps_gurtej_c0782510;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
